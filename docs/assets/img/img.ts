7fddf2c6499730cc627ebfaa81a4b66ab5443013
NelsonNikan <44512976+NelsonNikan@users.noreply.github.com>
2023-10-05 15:59:00 -0300

    deploy build path

---
+++ docs/assets/img/img.ts
@@ -0,0 +1,40 @@
+/**
+ * esse é um mock para simular o retorno de uma api com dados das imagens
+ * Aqui vamos evoluir adicionando rota de click e mais dados referente aos cards
+ */
+
+export const images = {
+    img: [
+        {
+        img:'1 (1).jpg',
+        cardInfo: {    
+        attraction: 'qwer',
+        country: 'rty7',
+        city: 'nvbn',
+        rating: '1',
+}},
+   {img:'1 (2).jpg', cardInfo: {    
+        attaction: 'dfghsd',
+        country: 'hgfjfgk',
+        city: 'il,yikms',
+        rating: '2',
+}},
+   {img:'1 (3).jpg', cardInfo: {    
+        attaction: 'asgsgh',
+        country: 'sgrthj',
+        city: 'gjntgukj',
+        rating: '3',
+}},
+    {img:'1 (4).jpg', cardInfo: {    
+        attaction: 'fasdfadh',
+        country: 'asdhah',
+        city: 'asgafhf',
+        rating: '4',
+}},
+     {img:'1 (5).jpg', cardInfo: {    
+        attaction: 'qwertqt',
+        country: 'qretqrtq',
+        city: 'qertqt',
+        rating: '5',
+}}
+  ]}
